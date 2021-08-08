5c0e3268cdf9ca732578317c786b4aed0458a083
Nelson Guterres <ashon2202@gmail.com>
2021-08-08 09:54:56 -0700

    Update Main.php

--- controllers/admin/Main.php
+++ controllers/admin/Main.php
@@ -1,1 +1,30 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
 
+class Main extends CI_Controller {
+
+
+	public function index()
+	{
+		if(isset($_GET['p'])){
+			$p = $_GET['p'];
+			$id = $_GET['id'];
+		}else{
+			$p='main';
+			$id='';
+		}
+		$data['pagina']=$p;		
+		$data['id']=$id;		
+		$this->load->view('www/v_index',array_merge($data));
+
+	}
+
+	public function k()
+	{
+	    p($this->db1->orderby('t_imagen','id_multikey','FA00000001','RAND()'));
+
+	}
+
+	
+
+}
